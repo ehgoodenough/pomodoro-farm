1ab6deadc9bad3120e031d29a13a172a84e91f98
Andrew McPherson <andrewmcp333@gmail.com>
2016-07-17 23:37:55 -0700

    Instantiated an empty repository.

---
+++ share.sh
@@ -0,0 +1,19 @@
+REPOSITORY_URL=$(node -p -e "require('./package.json').repository.url")
+
+git clone $REPOSITORY_URL --branch gh-pages shares
+
+node build --production
+
+mkdir -p ./shares/$1
+cp -r ./builds/web/* ./shares/$1
+
+git --git-dir=./shares/.git --work-tree=./shares add .
+git --git-dir=./shares/.git --work-tree=./shares commit -m "Pushed $1"
+git --git-dir=./shares/.git --work-tree=./shares push origin gh-pages
+
+rm -rf shares
+
+echo
+echo Share your build by going to:
+echo $REPOSITORY_URL/$1
+echo
