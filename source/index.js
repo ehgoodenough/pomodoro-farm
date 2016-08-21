b0e5a8c2a90c9b4c4559bedca6cd6ffa6240971e
Andrew McPherson <andrewmcp333@gmail.com>
2016-08-21 11:47:47 -0700

    Added a countdown.

--- source/index.js
+++ source/index.js
@@ -8,6 +8,15 @@ var render = ReactDOM.render(<Mount/>, document.getElementById("mount"))
 class Pomodoro {
     constructor() {
         console.log("starting pomodoro")
+        this.time = 25 * 1000
+    }
+    update(delta) {
+        this.time -= delta
+        if(this.time <= 0) {
+            console.log("done!")
+        } else {
+            console.log(this.time)
+        }
     }
 }
 
@@ -15,17 +24,27 @@ class Game {
     startPomodoro() {
         this.pomodoro = new Pomodoro()
     }
+    update(delta) {
+        if(this.pomodoro != undefined) {
+            this.pomodoro.update(delta)
+        }
+    }
 }
 
 window.game = new Game()
 
 var Loop = function(functionality) {
+    var oldnow = window.performance.now()
     window.setInterval(function() {
-        functionality()
+        var newnow = window.performance.now()
+        var delta = newnow - oldnow
+        functionality(delta)
+        oldnow = newnow
     }, 1000 / 60)
 }
 
 var loop = Loop(function(delta) {
+    game.update(delta)
     render.setState({
         game: game
     })
