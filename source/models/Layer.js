7b2c64dd56849124fa398ebf4ccdf29eaf2aaa29
Andrew McPherson <ehgoodenough@gmail.com>
2018-04-03 17:20:32 -0700

    Added multiple layers on top of each other.

--- source/models/Layer.js
+++ source/models/Layer.js
@@ -1,6 +1,7 @@
 export default class Layer {
     constructor(layer) {
         this.points = layer.points
+        this.color = layer.color
 
         this.x = Math.min(...this.points.map(point => point.x))
         this.y = Math.min(...this.points.map(point => point.y))
--- source/views/Layer.view.js
+++ source/views/Layer.view.js
@@ -4,9 +4,9 @@ import "views/Layer.view.less"
 
 export default function Layer(props) {
     return (
-        <svg className="Layer" style={{"left": props.layer.x + "em", "top": props.layer.y + "em"}} 
+        <svg className="Layer" style={{"left": props.layer.x + "em", "top": props.layer.y + "em"}}
             viewBox={`${props.layer.x} ${props.layer.y} ${props.layer.width} ${props.layer.height}`} xmlns="http://www.w3.org/2000/svg">
-            <polygon points={getPoints(props.layer.points)}/>
+            <polygon points={getPoints(props.layer.points)} fill={props.layer.color}/>
         </svg>
     )
 }
